ad5b2ac773ed2e1c475445cd5296da6bf6d6307f
Serge A. Levin <serge.levin.spb@gmail.com>
2018-01-13 17:30:29 +0300

    + Some inplace documentation

--- pyofd/providers/base.py
+++ pyofd/providers/base.py
@@ -13,14 +13,40 @@ import io
 
 
 class Base:
+    """Base class for simple sessionless OFD provider checker
+
+    Performs single HTTP GET request to OFD provider website and tries to load receipt details from response
+    """
+
     providerName = None
+    """Human-readable OFD provider name 
+    """
+
     urlTemplate = ''
+    """URL to test receipt for validity. It may contain py-format placeholders
+    for the fields identifying receipt.
+    
+    For any field with *name* field *q_name* is generated with URL-quoted value
+    
+    Used by @validate method
+    """
     requiredFields = ()
+    """Tuple of field names to be not None for the provider to try validating receipt
+    
+    Used by @is_candidate method 
+    """
 
     def __init__(self):
         pass
 
     def is_candidate(self, **kwargs):
+        """Checks whether receipt is handled by current OFD providers
+
+        Basic check requires all the fields specified in @requiredFields are not None
+
+        :param kwargs:  Receipt identification fields with their values
+        :return: Boolean value indicating whether receipt is candidate to be handled by current OFD provider
+        """
         for field in self.requiredFields:
             if field not in kwargs or kwargs[field] is None:
                 return False
@@ -36,6 +62,16 @@ class Base:
             taxpayer_id=None,
             purchase_date=None,
     ):
+        """ Tries to load receipt detail from OFD provider website
+
+        :param signature: Receipt signature (FPD in terms of Tax service of Russia)
+        :param total: Receipt total
+        :param cash_machine_no: Cash machine serian number (RN KKT)
+        :param receipt_no: Receipt fiscal number (FN)
+        :param taxpayer_id: Seller's taxpayer identifier (INN)
+        :param purchase_date: Purchase date and time
+        :return: List of items within receipt or None
+        """
         context = {k: v for k, v in locals().items() if k != 'self'}
 
         q_context = { ('q_' + k): _parse.quote(str(v)) for k, v in context.items() if v is not None}
@@ -59,8 +95,8 @@ class Base:
             return None
 
     def parse_response(self, data):
-        """
-        overridable to try parsing data, received from server
+        """overridable to try parsing data, received from server
+
         :param data: BytesIO with data to be parsed
         :return: list of pyofd.ReceiptEntry if data is recognised
         """
